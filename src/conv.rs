0a04dfa64f051fb8f3d0fca39de8ad4417f81929
Dlurak <84224239+Dlurak@users.noreply.github.com>
2024-03-17 17:30:22 +0100

    Add unit tests

--- src/conv.rs
+++ src/conv.rs
@@ -14,3 +14,54 @@ pub fn convert(input: &str, format: &ColorFormat) -> Result<String, ()> {
         Err(_) => Err(()),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn invalid_colors() {
+        assert_eq!(convert(&"invalid", &ColorFormat::Hex), Err(()));
+    }
+
+    #[test]
+    fn from_hex() {
+        let hex = "#ff0000";
+        assert_eq!(convert(&hex, &ColorFormat::Hex), Ok(hex.to_string()));
+        assert_eq!(
+            convert(&hex, &ColorFormat::Rgb),
+            Ok(String::from("rgb(255,0,0)"))
+        );
+        assert_eq!(
+            convert(&hex, &ColorFormat::Hsl),
+            Ok(String::from("hsl(0,100%,50%)"))
+        );
+    }
+
+    #[test]
+    fn from_rgb() {
+        let rgb = "rgb(255,0,0)";
+        assert_eq!(
+            convert(&rgb, &ColorFormat::Hex),
+            Ok(String::from("#ff0000"))
+        );
+        assert_eq!(
+            convert(&rgb, &ColorFormat::Hsl),
+            Ok(String::from("hsl(0,100%,50%)"))
+        );
+    }
+
+    #[test]
+    fn from_hsl() {
+        let hsl = "hsl(0,100%,50%)";
+        assert_eq!(convert(&hsl, &ColorFormat::Hsl), Ok(hsl.to_string()));
+        assert_eq!(
+            convert(&hsl, &ColorFormat::Hex),
+            Ok(String::from("#ff0000"))
+        );
+        assert_eq!(
+            convert(&hsl, &ColorFormat::Rgb),
+            Ok(String::from("rgb(255,0,0)"))
+        );
+    }
+}
